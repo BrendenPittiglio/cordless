1051c409a86653a3abff04cd234696d7400401d4
BrendenPittiglio <brenden.pittiglio@maine.edu>
2020-02-25 16:29:14 -0500

    initial commit

---
+++ TruthSpotter.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TruthSpotter {
+	
+	
+	private static void initializePlayers() {
+		System.out.println("Enter number of players (2-4)");
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		initializePlayers();
+	}
+}
